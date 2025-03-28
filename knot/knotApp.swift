91a9b93fb5756b1a8050741e3ec6b5a8991bca52
erics118 <52634785+erics118@users.noreply.github.com>
2025-03-28 16:18:53 -0400

    Initial Commit

---
+++ knot/knotApp.swift
@@ -0,0 +1,17 @@
+//
+//  knotApp.swift
+//  knot
+//
+//  Created by Eric Shen on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct knotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
